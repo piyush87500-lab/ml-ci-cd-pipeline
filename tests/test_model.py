5921b4b2daaf809c22c7f4dedd6c22ebe609e9d3
piyush87500-lab <jaat87500@gmail.com>
2025-08-25 11:31:02 +0530

    Commit 1.5

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,7 +4,7 @@ from sklearn.ensemble import RandomForestClassifier
 
 class TestModelTraining(unittest.TestCase):
     def test_model_training(self):
-        model = joblib.load('model/telecom_churn_model.pkl')
+        model = joblib.load("data/iris.csv")
         self.assertIsInstance(model, RandomForestClassifier)
         self.assertGreaterEqual(len(model.feature_importances_), 4)
 
